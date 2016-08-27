51c86bff20847126915d5022698995cb72160c38
pozharskiy <pozharskiy@Sergey-Pozharskiys-MacBook-Pro.local>
2016-08-28 02:54:32 +0300

    Sergey
    task

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+          d<-dim(x)[1]
+          inv <- matrix(1:d^2*NA,d,d)
+          set <- function(y) {
+            x <<- y
+            inv <<- matrix(1:d^2*NA,d,d)
+          }
+          get <- function() x
+          setinv <- function(invers) inv <<- invers
+          getinv <- function() inv
+          list(set = set, get = get,
+               setinv = setinv,
+               getinv = getinv)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinv()
+      if(!is.na(inv[1,1])) {
+        message("getting cached data")
+        return(inv)
+      }
+      data <- x$get()
+      inv <- solve(data)
+      x$setinv(inv)
+      inv
 }
